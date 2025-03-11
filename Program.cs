0b56a1b1e4637e8660f207e54d576f99f5d729d1
benyamin87salar <salarnasabbeni87@gmail.com>
2025-03-12 01:35:33 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+Console.Write("please enter a number :");
+
+byte time = byte.Parse(Console.ReadLine());
+
+if (time >= 0 && time <= 6)
+    Console.WriteLine("good morning");
+if (time >= 7 && time <= 12)
+    Console.WriteLine("good morning");
+if (time >= 13 && time <= 18)
+    Console.WriteLine("good evening");
+if (time >= 19 && time <= 24)
+    Console.WriteLine("good night");
+else
+    Console.WriteLine("number must be in 0 and 24");
